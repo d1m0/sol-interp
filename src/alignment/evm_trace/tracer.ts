8b6c0238bfa4c55493e5a404a6b3c08778028fd6
Dimitar Bounov <d1m0@protonmail.com>
2026-03-31 11:51:38 +0300

    Fixes #53

--- src/alignment/evm_trace/tracer.ts
+++ src/alignment/evm_trace/tracer.ts
@@ -6,7 +6,9 @@ import {
     StepVMState,
     EventDesc,
     BaseSolTxTracer,
-    TracerOpts
+    TracerOpts,
+    FoundryTxResult,
+    OPCODES
 } from "sol-dbg";
 import {
     addCallInfo,
@@ -23,6 +25,9 @@ import { TypedTransaction } from "@ethereumjs/tx";
 import { addSnapshotInfo, SnapshotInfo } from "./transformers/state_snapshot";
 import { ArtifactManager } from "../../interp/artifactManager";
 import { addCodeInfo, CodeInfo } from "./transformers/code";
+import { Block } from "@ethereumjs/block";
+import { StateManagerInterface } from "@ethereumjs/common";
+import { assert } from "../../utils";
 
 /**
  * Annotated evm step struct used for aligning traces.
@@ -46,6 +51,44 @@ export class EVMTracer extends BaseSolTxTracer<EVMStep, TracerContext> {
     constructor(opts: TracerOpts = {}) {
         // Artifact Manager not used in this tracer
         super(new ArtifactManager([]), { strict: false, foundryCheatcodes: false, ...opts });
+    }
+
+    async debugTx(
+        tx: TypedTransaction,
+        block: Block,
+        stateBefore: StateManagerInterface,
+        ctx: TracerContext
+    ): Promise<[EVMStep[], FoundryTxResult, StateManagerInterface, TracerContext]> {
+        const [trace, txRes, stateAfter, tracerCtx] = await super.debugTx(
+            tx,
+            block,
+            stateBefore,
+            ctx
+        );
+
+        // Fix-up last step to account for traces ending with a weird exception
+        const lastStep = trace[trace.length - 1];
+
+        // If the trace doesnt terminate with a known return or an exception, assume that it terminates with an unknown exception
+        if (lastStep.returnInfo === undefined && lastStep.exceptionInfo === undefined) {
+            assert(
+                !new Set([
+                    OPCODES.RETURN,
+                    OPCODES.REVERT,
+                    OPCODES.SELFDESTRUCT,
+                    OPCODES.REVERT
+                ]).has(lastStep.op.opcode),
+                `Unexpected last opcode with missing info {0}`,
+                lastStep.op.mnemonic
+            );
+            lastStep.exceptionInfo = {
+                excData: new Uint8Array(),
+                isImplicit: true,
+                correspCallIdx: -1
+            };
+        }
+
+        return [trace, txRes, stateAfter, tracerCtx];
     }
 
     async processRawTraceStep(
